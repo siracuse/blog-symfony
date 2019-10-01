b30d5063de3cc3a69e75f43ae619e68570316f8c
siracuse <siracuse.harichandra@gmail.com>
2019-10-01 15:17:48 +0300

    formulaire d'ajout d'un post (ancienne methode)

--- src/BlogBundle/Controller/FrontController.php
+++ src/BlogBundle/Controller/FrontController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use BlogBundle\Entity\Post;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class FrontController extends Controller {
 
@@ -17,17 +22,47 @@ class FrontController extends Controller {
     public function accueilAction() {
 
         $posts= $this->getDoctrine()->getManager()->getRepository('BlogBundle:Post')->findAll();
-        
+
         return $this->render('front/accueil.html.twig', [
             'posts' => $posts
         ]);
     }
 
-    // /**
-    //  * @Route ("/newPost", name="newPost")
-    //  */
-    // public function newPostAction() {
+    /**
+     * @Route ("/newPost", name="newPost")
+     */
+    public function newPostAction(Request $request) {
+        $post = new Post();
 
-    // }
+        $form = $this->get('form.factory')->createBuilder(FormType::class, $post)
+            ->add('title', TextType::class)
+            ->add('date', DateType::class)
+            ->add('author', TextType::class)
+            ->add('content', TextType::class)
+            ->add('save', SubmitType::class)
+            ->getForm()
+        ;
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($post);
+                $em->flush();
+            }
+
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+            $posts= $this->getDoctrine()->getManager()->getRepository('BlogBundle:Post')->findAll();
+
+            return $this->render('front/accueil.html.twig', [
+                'posts' => $posts
+            ]);
+        }
+
+        return $this->render ('front/newPost.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
 
 }
